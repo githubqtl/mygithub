8dcd840bf98f1801365b041bcb2fc35bc63e7bd2
githubqtl <568139989@qq.com>
2019-08-24 18:35:33 +0800

    upd

---
+++ test/src/main/java/com/bjsxt/service/impl/UsersServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bjsxt.service.impl;
+
+import com.bjsxt.service.UsersService;
+
+public class UsersServiceImpl implements UsersService{
+
+	@Override
+	public void selAll() {
+		System.out.println("woaini");
+		
+	}
+
+}
